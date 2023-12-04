429cc22e8fbf8d8bb21194e70ab7af29cc6a00da
Martin <martin_lenart@icloud.com>
2023-12-04 18:43:18 +0100

    Update WebApi Com

--- AppStudies/Program.cs
+++ AppStudies/Program.cs
@@ -31,6 +31,16 @@ builder.Services.AddRazorPages(o =>
 #region Injecting a dependency service
 //Add the QuoteServices used in the studies, simple singleton list, no database, 
 builder.Services.AddSingleton<IQuoteService, csQuoteService>();
+
+//Injected to demonstrate communication with external WebApi service
+builder.Services.AddHttpClient(name: "MusicWebApi", configureClient: options =>
+{
+    options.BaseAddress = Configuration.csAppConfig.WebApiBaseUri;
+    options.DefaultRequestHeaders.Accept.Add(
+        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(
+            mediaType: "application/json",
+            quality: 1.0));
+});
 #endregion
 
 var app = builder.Build();
